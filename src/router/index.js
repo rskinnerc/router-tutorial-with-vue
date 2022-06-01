b4780c59146fb1d17ff8810b33728e3857f38f4c
Ronald <rjskinnerc@unal.edu.co>
2022-05-31 21:42:17 -0500

    Add match all route (not found)

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Expenses from '../views/Expenses.vue'
 import Invoices from '../views/Invoices.vue'
+import NotFound from '../views/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,8 +22,14 @@ const router = createRouter({
           name: 'invoices',
           component: Invoices
         }
-      ]
+      ],
+
     },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: NotFound
+    }
   ]
 })
 
